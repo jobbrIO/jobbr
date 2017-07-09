1b8e6974f6f1c35107ac24e78bd9a2f8875512aa
Michael Schnyder <michael.schnyder@zuehlke.com>
2017-07-09 14:25:49 +0200

    fixed typo in files/folder

--- src/Jobbr.DevSupport.ReferencedVersionAsserter.Tests/NuSpecParsingTests.cs
+++ src/Jobbr.DevSupport.ReferencedVersionAsserter.Tests/NuSpecParsingTests.cs
@@ -21,5 +21,18 @@ namespace Jobbr.DevSupport.ReferencedVersionAsserter.Tests
             Assert.AreEqual(1, loader.Dependencies.Count);
             Assert.AreEqual("Microsoft.Owin.Host.HttpListener", loader.Dependencies[0].Name);
         }
+
+        [TestMethod]
+        public void SingleDependency_WhenLoaded_VersionRangeMatches()
+        {
+            var loader = new NuSpecDependencyParser("NuSpecHttpListenerDependency.nuspec");
+
+            Assert.AreEqual(1, loader.Dependencies.Count);
+            Assert.AreEqual("Microsoft.Owin.Host.HttpListener", loader.Dependencies[0].Name);
+
+            Assert.AreEqual(3, loader.Dependencies[0].MinVersion.Major);
+            Assert.AreEqual(4, loader.Dependencies[0].MaxVersion.Major);
+        }
+
     }
 }
