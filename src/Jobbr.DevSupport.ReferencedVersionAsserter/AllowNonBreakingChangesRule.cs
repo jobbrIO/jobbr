f421e89f2495500061be7c8e3d5a710b3eae6246
Michael Schnyder <michael.schnyder@zuehlke.com>
2017-07-10 17:49:39 +0200

    added rule to check for non breaking changes allowance

---
+++ src/Jobbr.DevSupport.ReferencedVersionAsserter/AllowNonBreakingChangesRule.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jobbr.DevSupport.ReferencedVersionAsserter
+{
+    public class AllowNonBreakingChangesRule : WildCharNuspecRuleBase
+    {
+        public AllowNonBreakingChangesRule(string packageName) : base(packageName)
+        {
+        }
+
+        protected override bool Validate(NuspecDependency nuspecDependency, NuspecDependency nuConfigPackage, out string message)
+        {
+            var specMin = nuspecDependency.MinVersion;
+            var specMax = nuspecDependency.MaxVersion;
+
+            if (specMax == null)
+            {
+                message = $"Cannot evaluate rule for '{nuspecDependency.Name}' if no max range is set";
+                return false;
+            }
+
+            if (specMax.Major == specMin.Major && !(specMax.Minor > specMin.Minor))
+            {
+                message = $"There package reference '{nuspecDependency.Name}' with version '{nuspecDependency.Version}' does not allow non-breaking changes";
+                return false;
+            }
+
+            message = null;
+            return false;
+        }
+    }
+}
